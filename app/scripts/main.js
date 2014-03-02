5cebdba1c03c4016a2f584b5b1ed46a879c2d731
Dominik Piatek <dominik.piatek@econsultancy.com>
2014-03-02 13:33:28 +0000

    Customize bootstrap collapse

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+(function () {
+
+  // DOM
+  var panelTitles = $(".panel-title");
+
+  // Event handler
+  var toggleOpenClass = function toggleOpenClass() {
+    var self = $(this);
+    self.toggleClass("-open");
+    panelTitles.not(self).removeClass("-open");
+  }
+
+  // Events
+  panelTitles.on("click", toggleOpenClass);
+})();
+
